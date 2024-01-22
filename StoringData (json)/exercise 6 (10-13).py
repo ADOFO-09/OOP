be582e3776ed95ad501b0f07c3a3f8b811aaafcf
ADOFO-09 <emmanueladofo011@gmail.com>
2024-01-22 19:39:57 +0000

    User Verification

--- StoringData (json)/exercise 6 (10-13).py
+++ StoringData (json)/exercise 6 (10-13).py
@@ -6,7 +6,12 @@ def get_stored_username():
     filename = 'StoringData (json)/username.json'
     with open(filename) as f_obj:
         username = json.load(f_obj)
+        checker = input("Is this username: " + username + " your name? (true/false)\n")
+# Responsible for checking username if the current user is not the one who last used the program
+    if checker == 'true':
         print("Welcome back " + username + "!")
+    elif checker == 'false':
+        get_new_username()
 
 # Responsible for getting new username and storing it
 def get_new_username():
@@ -16,6 +21,12 @@ def get_new_username():
     with open(filename, 'w') as f_obj:
         json.dump(username, f_obj)
         print("We will remember you when you come back "+ username + "!")
+
+
+    
+# Before printing a welcome back message in greet_user(), ask the user if 
+# this is the correct username. If it’s not, call get_new_username() to get the correct 
+# username
 
 # Prints an appropriate message: it either welcomes back an existing user or greets a new user
 def great_user():
