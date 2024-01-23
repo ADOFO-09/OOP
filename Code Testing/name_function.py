cac3d9a54981a4eaa176ee6f4f99c346e7390415
ADOFO-09 <emmanueladofo011@gmail.com>
2024-01-23 20:23:26 +0000

    A failed test and Responding to a failed test

--- Code Testing/name_function.py
+++ Code Testing/name_function.py
@@ -1,5 +1,14 @@
-def get_formatted_name(first, last):
+def get_formatted_name(first, last, middle=''):
     """Generate a neatly formatted name"""
-    full_name = first + ' ' + last
+# Responsible for formatting names with middle name
+    if middle:
+        full_name = first + ' ' + middle + ' ' + last
+
+# Responsible for formatting names with a first and last name
+    else:
+        full_name = first + ' ' + last
     return full_name.title()
 
+
+# In response to a failed test, don't change the test. Instead
+# fix the code that caused the test to fail.
--- Code Testing/test_name_function.py
+++ Code Testing/test_name_function.py
@@ -2,9 +2,19 @@ import unittest
 from name_function import get_formatted_name
 
 class NamesTestCase(unittest.TestCase):
+# The method below tests for two names that is first and last names
     def test_first_last_name(self):
-        """Do names like Emmanuel Adofo exist """
-        formatted_name = get_formatted_name("Emmanuel","Adofo")
-        self.assertEqual(formatted_name, "Emmanuel Adofo")
+        """Do names like 'Emmanuel Adofo' exist """
+        formatted_name = get_formatted_name('emmanuel','adofo')
+        self.assertEqual(formatted_name, 'Emmanuel Adofo')
+    
+# The method below tests for three names that are first last and middle
+    def test_first_last_middle_name(self):
+        """Do name like 'Asante Mensah Yussif' works"""
+        formatted_name = get_formatted_name('asante','yussif','mensah')
+        self.assertEqual(formatted_name, 'Asante Mensah Yussif')
 
 unittest.main()
+
+
+
