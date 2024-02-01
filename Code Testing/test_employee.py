2c0b560b296893409309d7490fd7c451e099c399
ADOFO-09 <emmanueladofo011@gmail.com>
2024-02-01 22:10:33 +0000

    Added setUp() method to test_employee

--- Code Testing/test_employee.py
+++ Code Testing/test_employee.py
@@ -1,18 +1,19 @@
 import unittest
 from employee import Employee
 
-class TestEmployee(unittest.TestCase):   
+class TestEmployee(unittest.TestCase):
+# This method creates an employee instance
+    def setUp(self):
+        self.employee = Employee("Emmanuel","Adofo")   
 
 # This method tests for default_raise in salary
     def test_give_default_raise(self):
-       self.employee = Employee("Emmanuel","Adofo")
        self.employee.give_raise()
        expected_salary = self.employee.default_raise + 8000
        self.assertEqual(self.employee.annual_salary, expected_salary)
 
 # This method tests for custom raise in salary
     def test_give_custom_raise(self):
-        self.employee = Employee("Emmanuel", "Adofo")
         custom_raise = 5000
         self.employee.give_raise()
         expected_salary =  5000 + custom_raise
