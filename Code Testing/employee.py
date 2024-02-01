b58ce6959a8c48bfe406a142d5d911c7687b711d
ADOFO-09 <emmanueladofo011@gmail.com>
2024-02-01 16:19:46 +0000

    Solved logical error

--- Code Testing/employee.py
+++ Code Testing/employee.py
@@ -8,19 +8,20 @@
 # your test case, and make sure both tests pass
 
 class Employee():
-    def __init__(self,first_name, last_name, default_raise, annual_salary= 10000):
+    def __init__(self,first_name, last_name, default_raise = 2000, annual_salary= 10000):
         self.first_name = first_name
         self.last_name = last_name
         self.default_raise = default_raise
         self.annual_salary = annual_salary
 
-    def give_raise(self, custom_raise):
-        default_salary_raise = self.annual_salary + self.default_raise
-        print("The current salary is $" + str(default_salary_raise))
-        self.custom_raise = custom_raise
-        new_custom_salary = self.annual_salary + custom_raise
-        print("The new salary is $" + str(new_custom_salary))
-
+# Responsible for adding amount to annual salary
+    def give_raise(self):
+        self.custom_amount = 5000
+        current_salary = self.annual_salary + self.default_raise
+        print("The current salary is " + str(current_salary))
+        print("Custom amount is " + str(self.custom_amount + self.annual_salary))
+    
+    
 # emp1 = Employee("Emmanuel","Adofo", 150)
 # print(emp1.give_raise())
 
--- Code Testing/test_employee.py
+++ Code Testing/test_employee.py
@@ -2,17 +2,19 @@ import unittest
 from employee import Employee
 
 class TestEmployee(unittest.TestCase):   
-    
+
+# This method tests for default_raise in salary
     def test_give_default_raise(self):
-       self.employee = Employee("Emmanuel","Adofo", 5000)
+       self.employee = Employee("Emmanuel","Adofo")
        self.employee.give_raise()
-       expected_salary = self.employee.default_raise + 5000
+       expected_salary = self.employee.default_raise + 8000
        self.assertEqual(self.employee.annual_salary, expected_salary)
 
-    def test_give_default_raise(self):
-        self.employee = Employee("Emmanuel", "Adofo", 5000)
+# This method tests for custom raise in salary
+    def test_give_custom_raise(self):
+        self.employee = Employee("Emmanuel", "Adofo")
         custom_raise = 5000
-        self.employee.give_raise(custom_raise)
+        self.employee.give_raise()
         expected_salary =  5000 + custom_raise
         self.assertEqual(self.employee.annual_salary, expected_salary)  
 
